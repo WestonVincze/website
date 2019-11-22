53fff5860390ce95565be6fd2b7062ff51b7857d
Weston Vincze <westonvincze@gmail.com>
2019-11-22 17:16:12 -0500

    adds new fonts, boostrap and global css to layout

--- components/layout.js
+++ components/layout.js
@@ -2,14 +2,47 @@ import Head from 'next/head';
 import Navbar from '../components/navbar';
 
 const Layout = (props) => (
-    <div>
+    <div className="main">
         <Head>
             <title>Weston Vincze</title>
+            <link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Great+Vibes|Zeyada" rel="stylesheet"></link>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
         </Head>
         <Navbar/>
-        {props.children}
-        <style jsx>{`
+        <div className="folder">
+            {props.children}
+        </div>
+        
+        <style jsx global>{`
+            @font-face {
+                font-family: 'typewriter';
+                src: url('/assets/fonts/fzm-Old.Typewriter.ttf') format('truetype');
+            }
+
+            body {
+                background-color: #cce6ff;
+                font-family: 'Roboto Slab', serif;
+                font-family: 'typewriter', sans-serif;
+            }
+
+            .folder {
+                background-color: #f3dca3;
+                padding-top: 250px;
+                margin: 0 0.5%;
+            }
             
+            .written {
+                color: #00407e;
+                font-family: 'Zeyada', cursive;
+            }
+
+            @media screen and (max-width: 768px) {=
+
+                h2 {
+                    font-size: 1.5em;
+                }
+
+            }
         `}
         </style>
     </div>
