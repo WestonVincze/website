a42e0d8b2ec8eb63ac09094f479df241f126e084
Weston Vincze <westonvincze@gmail.com>
2019-11-22 17:17:13 -0500

    adds navbar content and jsx for a fixed style

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,68 @@
 import Link from 'next/link';
+import StickyTitle from '../components/stickyTitle';
 
 const Navbar = () => (
-    <nav id="main_navbar">
-        <ul className="test">
-            <li><Link href="/"><a>About Me</a></Link></li>
-            <li><Link href="/projects"><a>Projects</a></Link></li>
-            <li><Link href="/skills"><a>Skills</a></Link></li>
-        </ul>
-    </nav>
+    <div className="stickyNav">
+        <nav>
+            <ul>
+                <li><Link href="/"><a className="active">About Me</a></Link></li>
+                <li><Link href="/projects"><a>Projects</a></Link></li>
+                <li><Link href="/skills"><a>Skills</a></Link></li>
+            </ul>
+        </nav>
+        <StickyTitle/>
+        
+        <style jsx>{`
+                .stickyNav {
+                    position: fixed;
+                    z-index: 99;
+                    width: 100%;
+                    padding: 0 0.5%;
+                }
+
+                nav { 
+                    background-color: #cce6ff;
+                    position: sticky;
+                    top: 0;
+                    z-index: 10;
+                }
+
+                nav ul {
+                    list-style-type: none;
+                    margin: 0;
+                    padding: 0;
+                    overflow: hidden;
+                }
+
+                nav ul li {
+                    float: right;
+                    border-radius: 7px 7px 0px 0px;
+                    margin-left: 2px;
+                }
+
+                nav ul li a {
+                    display: block;
+                    text-align: center;
+                    padding: 10px 12px;
+                    text-decoration: none;
+                    border-radius: 5px 5px 0px 0px;
+                    margin-top: 4px;
+                    color: inherit; 
+                    background-color: #f1d592;
+                }
+
+                nav ul li a:hover {
+                    margin-top: 0;
+                    padding-bottom: 14px;
+                    color: inherit;
+                }
+
+                nav ul li a.active {
+                    background-color: #f3dca3;
+                }
+            `}
+            </style>
+    </div>
 );
 
 export default Navbar;
