5f75482799249ea9b4bfda242ecfc1f524592feb
Weston Vincze <westonvincze@gmail.com>
2019-11-24 12:56:11 -0500

    adds a new link component with an activeClass property

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,13 @@
-import Link from 'next/link';
+import Link from '../components/link';
 import StickyTitle from '../components/stickyTitle';
 
 const Navbar = () => (
     <div className="stickyNav">
         <nav>
             <ul>
-                <li><Link href="/"><a className="active">About Me</a></Link></li>
-                <li><Link href="/skills"><a>Skills</a></Link></li>
-                <li><Link href="/projects"><a>Projects</a></Link></li>
+                <li><Link activeClassName="active" href="/"><a>About Me</a></Link></li>
+                <li><Link activeClassName="active" href="/skills"><a>Skills</a></Link></li>
+                {/* <li><Link activeClassName="active" href="/projects"><a>Projects</a></Link></li> */}
             </ul>
         </nav>
         <StickyTitle/>
@@ -22,7 +22,7 @@ const Navbar = () => (
 
                 nav { 
                     background-color: #cce6ff;
-                    background-color: #5b5d5c;
+                    background-color: #fd8469;
                     position: sticky;
                     top: 0;
                     z-index: 10;
@@ -48,12 +48,14 @@ const Navbar = () => (
                     padding: 10px 12px;
                     text-decoration: none;
                     border-radius: 5px 5px 0px 0px;
+                    transition: margin, 0.2s;
                     margin-top: 4px;
                     color: inherit; 
                     background-color: #f1d592;
                 }
 
                 nav ul li a:hover {
+                    transition: margin, 0.2s;
                     margin-top: 0;
                     padding-bottom: 14px;
                     color: inherit;
